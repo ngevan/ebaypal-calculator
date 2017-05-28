ef33f1d81ca37dbd00311ff518c7d78ef07412df
evanscloud <dijkstrascloud@gmail.com>
2017-05-28 17:47:41 -0400

    Import CalculatorDashboard component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
+import CalculatorDashboard from './components/CalculatorDashboard';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div>
         <Header />
+        <CalculatorDashboard />
       </div>
     );
   }
